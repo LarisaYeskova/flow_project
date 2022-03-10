c85e066c02d7de11ba8bbbe214208d511ee2b201
YeskovaLarysa <yeskova.larysa@gmail.com>
2022-03-11 01:21:01 +0200

    fix
    пофиксила ошибку отображения

--- components/ProblemSolution.jsx
+++ components/ProblemSolution.jsx
@@ -4,12 +4,12 @@ export default function ProblemSolution() {
   return (
     <div className={styles.problemSolution_wrap}>
       <div className={styles.ProblemSolution}>
-        <p>The Problem</p>
-        <img src="./public/ProblemSolution.png" />
-        <p>The Solution</p>
+        <p className={styles.problemSolution_text}>The Problem</p>
+        <img src="./ProblemSolution.png" />
+        <p className={styles.problemSolution_text}>The Solution</p>
       </div>
 
-      <div className={styles.problemSolution_text}>
+      <div className={styles.problemSolution_paregraph}>
         <p>
           Most student-athletes don't have a legal, compliant, and fair way to
           connect with 3rd parties to capitalize on opportunities afforded by
