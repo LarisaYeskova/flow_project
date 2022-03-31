1dfbd15fc7bb22c92ec75ba10146fbae5cb8eb6a
YeskovaLarysa <yeskova.larysa@gmail.com>
2022-03-31 20:36:41 +0300

    little fix

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Opportunity from "../components/opportunity/Opportunity";
 import ProblemSolution from "../components/problemSolution/ProblemSolution";
 import TextBlock from "../components/textBlock/TextBlock";
 import Covered from "../components/covered/Covered";
-import Connecting from "../components/Connecting/Connecting";
+import Connecting from "../components/connecting/Connecting";
 import Video from "../components/video/Video";
 import AnyQues from "../components/anyQuestion/AnyQues";
 import Team from "../components/team/Team";
